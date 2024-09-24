6d3645b98399ec5fdf2c7a6ac03dd528d03cd529
JIUHWA <67797035+IIT25@users.noreply.github.com>
2024-09-24 19:57:09 +0200

    measure circ fix

--- qcbm/circuit.py
+++ qcbm/circuit.py
@@ -8,7 +8,8 @@ from qiskit.transpiler.preset_passmanagers import generate_preset_pass_manager
 def create_circ(n, samples):
     qc = QuantumCircuit(n)
     add_entangling_layer(qc, get_tree_representation(samples))
-
+    qc.measure_all()
+    measure_circ(qc,100)
 
 def create_qcbm(n, samples):
     pairs = [[pair[0], pair[1]] for pair in get_tree_representation(samples)]
@@ -16,17 +17,16 @@ def create_qcbm(n, samples):
     circuit = EfficientSU2(n, entanglement=pairs, reps=1)
     return circuit
 
-    qc.measure_all()
-    measure_circ(qc,100)
+    
 
 def add_entangling_layer(qc, tree):
     for pair in tree:
         qc.cx(qc.qubits[pair[0]], qc.qubits[pair[1]])
     print(qc.data)
 def measure_circ(qc, N):
-    sampler = StatevectorSampler(shots=N)
+    sampler = StatevectorSampler()
     pm = generate_preset_pass_manager(optimization_level=1)
-    result = sampler.run([qc]).result()
+    result = sampler.run([qc], shots=N).result()
     data_pub = result[0].data
     bitstrings = data_pub.meas.get_bitstrings()
     print(f"The number of bitstrings is: {len(bitstrings)}")
