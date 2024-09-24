8961b674115d6d8abd41feed5274cd66287adf20
Sooki <0xsooki@protonmail.com>
2024-09-24 19:55:04 +0300

    update imports

--- qcbm/circuit.py
+++ qcbm/circuit.py
@@ -1,5 +1,6 @@
 from qiskit import QuantumCircuit
-from chow_liu_tree import get_tree_representation
+from qcbm.chow_liu_tree import get_tree_representation
+from qiskit.circuit.library import EfficientSU2
 
 
 def create_circ(n, samples):
@@ -7,7 +8,13 @@ def create_circ(n, samples):
     add_entangling_layer(qc, get_tree_representation(samples))
 
 
+def create_qcbm(n, samples):
+    pairs = [[pair[0], pair[1]] for pair in get_tree_representation(samples)]
+    print(len(pairs))
+    circuit = EfficientSU2(n, entanglement=pairs, reps=1)
+    return circuit
+
+
 def add_entangling_layer(qc, tree):
     for pair in tree:
         qc.cx(qc.qubits[pair[0]], qc.qubits[pair[1]])
-    print(qc.data)
