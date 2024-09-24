727ee519620176632d74d30c356f38bca0fe1d63
JIUHWA <67797035+IIT25@users.noreply.github.com>
2024-09-24 19:33:22 +0200

    sampler

--- qcbm/circuit.py
+++ qcbm/circuit.py
@@ -1,13 +1,24 @@
 from qiskit import QuantumCircuit
-from chow_liu_tree import get_tree_representation
+from qiskit.primitives import StatevectorSampler
+from qiskit.transpiler.preset_passmanagers import generate_preset_pass_manager
 
 
 def create_circ(n, samples):
     qc = QuantumCircuit(n)
     add_entangling_layer(qc, get_tree_representation(samples))
-
+    qc.measure_all()
+    measure_circ(qc,100)
 
 def add_entangling_layer(qc, tree):
     for pair in tree:
         qc.cx(qc.qubits[pair[0]], qc.qubits[pair[1]])
     print(qc.data)
+def measure_circ(qc, N):
+    sampler = StatevectorSampler(shots=N)
+    pm = generate_preset_pass_manager(optimization_level=1)
+    result = sampler.run([qc]).result()
+    data_pub = result[0].data
+    bitstrings = data_pub.meas.get_bitstrings()
+    print(f"The number of bitstrings is: {len(bitstrings)}")
+    counts = data_pub.meas.get_counts()
+    print(f"The counts are: {counts}")
