2f37e2cbdf8516abcecd70aff46b3b8af93fdbc4
Sooki <0xsooki@protonmail.com>
2024-09-24 19:25:17 +0300

    update gitignore & structure

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import circuit as circ
+import qcbm.circuit as circ
 import numpy as np
-arr = np.array([[0,1,0, 0,0], [0,1,1, 0, 1], [1,1,1, 1, 1]])
+arr = np.array([[0, 1, 0, 0, 0], [0, 1, 1, 0, 1], [1, 1, 1, 1, 1]])
 circ.create_circ(5, arr)
